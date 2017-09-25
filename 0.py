dc6a639cc7cb24b57f919393a5c0928db418957f
Adam <acdoyle630@gmail.com>
2017-09-25 16:36:01 -0700

    lesson 0

--- 0.py
+++ 0.py
@@ -3,3 +3,7 @@
 #Asks user to enter his/her last name
 #Prints user's last mane and DOB in that order
 
+userLastName = raw_input("Enter your last : ")
+userDOB = raw_input("Enter your date of birth: ")
+
+print "User: " + userLastName + " Born: " + userDOB
